5d4b691726801a03a34ea7069b21aa179683f9a7
Sha256_Nulled <Fathindos.FD@gmail.com>
2024-10-18 03:03:42 +0300

    feat: Add cool_script.py to generate and print random quotes

---
+++ cool_script.py
@@ -0,0 +1,15 @@
+import random
+
+quotes = [
+    "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "The best way to predict the future is to invent it. - Alan Kay",
+    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+    "The only way to do great work is to love what you do. - Steve Jobs"
+]
+
+def get_random_quote():
+    return random.choice(quotes)
+
+if __name__ == "__main__":
+    print(get_random_quote())
