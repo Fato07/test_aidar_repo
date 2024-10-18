01e565662ef8bc4ce9e7cb44dbe25485cd9d0120
Sha256_Nulled <Fathindos.FD@gmail.com>
2024-10-18 03:08:54 +0300

    feat: Add new quote from Theodore Roosevelt

--- cool_script.py
+++ cool_script.py
@@ -5,7 +5,8 @@ quotes = [
     "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
     "The best way to predict the future is to invent it. - Alan Kay",
     "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-    "The only way to do great work is to love what you do. - Steve Jobs"
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Believe you can and you're halfway there. - Theodore Roosevelt"
 ]
 
 def get_random_quote():
